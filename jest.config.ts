cfd31336dc5ce67c92b56ec1071175382fd4cab4
Trevor Scheer <trevor.scheer@gmail.com>
2022-03-02 14:32:13 -0800

    Prettier

--- jest.config.ts
+++ jest.config.ts
@@ -10,7 +10,7 @@ const config: Config.InitialOptions = {
       tsconfig: 'tsconfig.test.json',
     },
   },
-  testRegex: '/__tests__/.*\.test\.ts$',
+  testRegex: '/__tests__/.*.test.ts$',
   verbose: true,
 };
 
